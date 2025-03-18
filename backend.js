a531b8d17d9fc385aa6a241edfbecaed2e296ad2
WindowsXpguy <theaverageuser404@gmail.com>
2025-03-18 06:24:58 -0400

    Create backend.js

---
+++ backend.js
@@ -0,0 +1,8 @@
+console.log("Backend script loaded from GitHub Pages!");
+
+function backendFunction() {
+    document.getElementById("output").innerText = "Hello from the external script!";
+}
+
+// Run the function automatically when the script loads
+backendFunction();
